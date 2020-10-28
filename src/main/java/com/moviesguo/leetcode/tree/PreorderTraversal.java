809f54218833e6d83e3e7e328e42c9368a639e55
guotianxiao <guotianxiao@mbcore.com>
2020-10-28 10:04:51 +0800

    每日一题 144. 二叉树的前序遍历

--- src/main/java/com/moviesguo/leetcode/tree/PreorderTraversal.java
+++ src/main/java/com/moviesguo/leetcode/tree/PreorderTraversal.java
@@ -6,6 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * 144. 二叉树的前序遍历
+ * 给定一个二叉树，返回它的前序遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,2,3]
+ * 进阶:递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
 public class PreorderTraversal {
 
     public List<Integer> preorderTraversal(TreeNode root) {
