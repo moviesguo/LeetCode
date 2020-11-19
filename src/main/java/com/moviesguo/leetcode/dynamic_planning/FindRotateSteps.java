26d714f50847844a8847f1809d015ae4b0d5b9f5
guotianxiao <guotianxiao@mbcore.com>
2020-11-19 10:26:20 +0800

    每日一题 283. 移动零

--- src/main/java/com/moviesguo/leetcode/dynamic_planning/FindRotateSteps.java
+++ src/main/java/com/moviesguo/leetcode/dynamic_planning/FindRotateSteps.java
@@ -1,13 +1,27 @@
 package com.moviesguo.leetcode.dynamic_planning;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class FindRotateSteps {
 
+    public static void main(String[] args) {
+        FindRotateSteps findRotateSteps = new FindRotateSteps();
+        findRotateSteps.findRotateSteps("", "");
+    }
+
     public int findRotateSteps(String ring, String key) {
         HashMap<String, Integer>[] distance = new HashMap[ring.length()];
+        Queue<Integer> queue = new LinkedList<>();
+        int[][] ans = new int[3][3];
+        for (int[] an : ans) {
+            Arrays.fill(an,0);
+        }
+        for (int[] an : ans) {
+            for (int i : an) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
         return 0;
     }
 
